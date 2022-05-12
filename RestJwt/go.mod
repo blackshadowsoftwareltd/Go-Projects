d2f39464600fad4c2b557a3043b1b2350edbadfc
remon <blackshadowsoftwareltd@gmail.com>
2022-05-13 04:07:37 +0600

    func

--- RestJwt/go.mod
+++ RestJwt/go.mod
@@ -1,3 +1,5 @@
 module TestJwt
 
 go 1.18
+
+require github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
--- RestJwt/main.go
+++ RestJwt/main.go
@@ -1,3 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"time"
+
+	jwt "github.com/dgrijalva/jwt-go"
+)
+
 func main() {}
+
+func GenerateJWT() (string error) {
+	token := jwt.New(jwt.SigningMethodES256)
+	fmt.Println("token:", token)
+	claims := token.Claims.(jwt.MapClaims)
+
+	claims["authorized"] = true
+	claims["user"] = "Remon"
+	claims["exp"] = time.Now().Add(time.Minute * 30).Unix()
+
+}
