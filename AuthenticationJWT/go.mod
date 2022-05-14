f59754f489d7a52955980321ac8e2ea6b33abba8
remon <blackshadowsoftwareltd@gmail.com>
2022-05-15 00:48:09 +0600

    init

---
+++ AuthenticationJWT/go.mod
@@ -0,0 +1,5 @@
+module AuthenticationJWT
+
+go 1.18
+
+require github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
---
+++ AuthenticationJWT/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	jwt "github.com/dgrijalva/jwt-go"
+	"net/http"
+	"time"
+)
+
+ func main(){
+	 
+ }
