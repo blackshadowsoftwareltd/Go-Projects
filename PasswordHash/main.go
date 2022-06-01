cf1542300b394f8ba8bf0de648bd574a33cd4ef2
remon <blackshadowsoftwareltd@gmail.com>
2022-06-01 07:39:51 +0600

    password hash

---
+++ PasswordHash/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+
+	bcrypt "golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
+
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
+
+func main() {
+	password := "secret"
+	newPassword := "secret"
+	hash, _ := HashPassword(password) // ignore error for the sake of simplicity
+
+	fmt.Println("Password:", password)
+	fmt.Println("Hash:    ", hash)
+
+	match := CheckPasswordHash(newPassword, hash)
+	fmt.Println("Match:   ", match)
+}
