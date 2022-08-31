5fbed3348d818e6ddd1f79aec4b2efbca4ea7707
remon <blackshadowsoftwareltd@gmail.com>
2022-08-31 23:02:28 +0600

    comments

--- GoWithPostgre/database/functions/create.go
+++ GoWithPostgre/database/functions/create.go
@@ -5,15 +5,17 @@ import (
 	"fmt"
 )
 
+///? create a table in database with a auto-increment primary key
 func createDBTable(tableName string) error {
+	///? CREATE TABLE table_name(); to create a table
+	///? ALTER TABLE table_name ADD COLUMN id SERIAL PRIMARY KEY; to add a auto-increment primary key
 	insertStatement := fmt.Sprintf("CREATE TABLE %s();ALTER TABLE %s ADD COLUMN id SERIAL PRIMARY KEY;", tableName, tableName)
+
 	fmt.Println(insertStatement)
 	_, err := databases.DB.Query(insertStatement)
 	if err != nil {
 		fmt.Println("Error", err)
-
 		return err
 	}
 	return nil
-
 }
