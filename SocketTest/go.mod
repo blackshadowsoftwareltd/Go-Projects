8532979565e20f2479dd7eadf245bc7c0b6885b6
remon <blackshadowsoftwareltd@gmail.com>
2022-12-21 17:02:45 +0600

    add websocket.io

--- SocketTest/go.mod
+++ SocketTest/go.mod
@@ -1,3 +1,10 @@
 module SocketTest
 
 go 1.18
+
+require (
+	github.com/gofrs/uuid v4.0.0+incompatible // indirect
+	github.com/gomodule/redigo v1.8.4 // indirect
+	github.com/googollee/go-socket.io v1.6.2 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+)
--- SocketTest/main.go
+++ SocketTest/main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
 
+	socketio "github.com/googollee/go-socket.io"
+)
+
+func main() {
+	fmt.Println("Started")
+	server := socketio.NewServer(nil)
+
+	server.OnConnect("/", func(s socketio.Conn) error {
+		s.SetContext("")
+		fmt.Println("connected:", s.ID())
+		return nil
+	})
+	go server.Serve()
+	defer server.Close()
+
+	http.Handle("/socket.io/", server)
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
