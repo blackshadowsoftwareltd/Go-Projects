80afdd1eb034c17f28b0aa52a5c63ad54cad989d
Remon <blackshadowsoftwareltd@gmail.com>
2023-01-08 20:49:00 +0600

    try another way

--- GinTest/main.go
+++ GinTest/main.go
@@ -1,11 +1,17 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.String(200, "Hello, World!")
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
 	})
-	r.Run(":8080")
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
