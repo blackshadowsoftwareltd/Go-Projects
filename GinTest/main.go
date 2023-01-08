94906a15ba80c5126c528afb1054d768fa9171f9
Remon <blackshadowsoftwareltd@gmail.com>
2023-01-08 20:47:11 +0600

    fix. gin working

--- GinTest/main.go
+++ GinTest/main.go
@@ -1,9 +1,11 @@
 package main
 
-import (
-	"fmt"
-)
+import "github.com/gin-gonic/gin"
 
 func main() {
-	fmt.Println("Started")
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.String(200, "Hello, World!")
+	})
+	r.Run(":8080")
 }
