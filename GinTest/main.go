588f6249fc6dcdfcbfce38113ddc0ad5f8e0cdc2
Remon <blackshadowsoftwareltd@gmail.com>
2023-01-08 21:12:25 +0600

    get albut by id

--- GinTest/main.go
+++ GinTest/main.go
@@ -11,6 +11,7 @@ func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
 	router.POST("/albums", postAlbums)
+	router.GET("/albums/:id", getAlbumByID)
 
 	router.Run("localhost:8080")
 }
@@ -50,4 +51,23 @@ func postAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusCreated, albums)
 }
 
+// getAlbumByID locates the album whose ID value matches the id
+// parameter sent by the client, then returns that album as a response.
+func getAlbumByID(c *gin.Context) {
+	id := c.Param("id")
+
+	// Loop over the list of albums, looking for
+	// an album whose ID value matches the parameter.
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+}
+
+/// GET POST
 // http://localhost:8080/albums
+/// GET
+// http://localhost:8080/albums/2
